6a0754d84e0dffd851f690b439ff0afd3ae89e29
Xiao Jiang <showbufire@gmail.com>
2015-05-31 12:01:22 -0700

    make instruction an enumerable

--- instruction.rb
+++ instruction.rb
@@ -1,20 +1,32 @@
 require './token.rb'
 
 class Instruction
+  include Enumerable
+  
   def to_s
-    "#{self.class} @#{lineno}"
+    children = map{|x| x.value}
+    "#{self.class} @#{lineno} [#{children.join(",")}]"
+  end
+
+  def each(&block)
+    to_enum :tokens if block.nil?
+    tokens(&block)
+  end
+
+  def lineno
+    first.lineno
   end
 end
 
 class AInstruction < Instruction
   attr_reader :addr
-  
+
   def initialize(addr)
     @addr = addr
   end
 
-  def lineno
-    @addr.lineno
+  def tokens
+    yield addr
   end
 end
 
@@ -30,6 +42,12 @@ class CInstruction < Instruction
   def lineno
     @comp_tokens[0].lineno if @comp_tokens.length > 0
   end
+
+  def tokens
+    yield dest_symbol unless dest_symbol.nil?
+    comp_tokens.each {|token| yield token}
+    yield jump_symbol unless jump_symbol.nil?
+  end
 end
 
 class LabelInstruction < Instruction
@@ -39,7 +57,7 @@ class LabelInstruction < Instruction
     @label = label
   end
 
-  def lineno
-    @label.lineno
+  def tokens
+    yield label
   end
 end
